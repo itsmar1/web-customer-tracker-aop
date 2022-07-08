e6d3f34bb6cd2a96d02f4a85bba07a436e7002ef
Marouane Ahal <d144013152@gmail.com>
2022-07-08 22:09:45 +0100

    add logging support

--- src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -3,11 +3,15 @@ package com.luv2code.springdemo.aspect;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Logger;
+
 @Aspect
 @Component
 public class CRMLoggingAspect {
 
     // setup logger
+    private Logger myLogger = Logger.getLogger(getClass().getName());
+    
     // setup pointcut declarations
     // add @Before advice
     // add @AfterReturning advice
