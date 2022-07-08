85914fc2d51451311ea5de91f9e5fce28081a769
Marouane Ahal <d144013152@gmail.com>
2022-07-08 21:28:39 +0100

    add AspectJ jar file

---
+++ src/com/luv2code/testdb/TestDbServlet.java
@@ -0,0 +1,43 @@
+package com.luv2code.testdb;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet(name = "TestDbServlet", value = "/TestDbServlet")
+public class TestDbServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // setup connection variables
+        String user = "springstudent";
+        String pass = "springstudent";
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+        String driver = "com.mysql.cj.jdbc.Driver";
+
+        // get connection to database
+        try {
+            PrintWriter out = response.getWriter();
+
+            out.println("Connecting to database: " + jdbcUrl);
+
+            Class.forName(driver);
+
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+
+            out.println("SUCCESS!!!");
+
+            myConn.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ServletException(e);
+        }
+
+    }
+}
